af84827738cfb8e30dda0b3268e720c9b2d6f12f
Art Beckman <aabeckman@yahoo.com>
2015-11-16 20:56:48 +0000

    fixed hilite

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,17 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    sort = params[:sort] || session[:sort]
+    case sort
+    when 'title'
+      ordering = {:title => :asc}
+      @title_header = 'hilite'
+    when 'release_date'
+      ordering = {:release_date => :asc}
+      @date_header = 'hilite'
+    end
+
+    @movies = Movie.all.order(ordering)
   end
 
   def new
